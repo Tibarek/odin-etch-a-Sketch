bd529a2ca8d0636c7278f0672805814c80c569e2
Tibarek Yuggie <ttibarek@gmail.com>
2025-05-30 10:39:10 +0300

    UPDATE CSS TO MAKE THE GRID LOOK BETTER
    ALIGN THE GRID TO CENTER
    ADD CSS 'SQUARE' CLASS TO JS
    UPDATE THE STYLE TO MAKE GRID ON CENTER

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const container = document.querySelector(".container");
 
 function grid(row, column){
     for(let i = 0; i < column; i++){
-        let line = document.createElement("div");
+        let squareRow = document.createElement("div");
         for(let i = 0; i < row; i++){
             let square = document.createElement("div");
-            square.textContent = "I am a div";
-            line.appendChild(square);
+            square.classList.add("square");
+            squareRow.appendChild(square);
         }
-        container.appendChild(line);
+        container.appendChild(squareRow);
     }
 }
 
