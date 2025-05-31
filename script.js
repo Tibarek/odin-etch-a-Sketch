75d2381caa345ca1c95b52057a9785d232b9c2bb
Tibarek Yuggie <ttibarek@gmail.com>
2025-05-31 10:25:36 +0300

    MAKE WIDTH AND HIGHT OF EACH SQUARE WITH JS
    CALCULATE THE WIDTH OF THE CONTAINER WITH GRID SIZE TO FIT TO CONTAINER PIXEL.
    USE BOX SIZING TO AVOID OVERFLOWING

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const container = document.querySelector(".container");
+const gridBtn = document.querySelector(".grid-btn");
 
-function grid(row, column){
-    for(let i = 0; i < column; i++){
-        let squareRow = document.createElement("div");
-        for(let i = 0; i < row; i++){
-            let square = document.createElement("div");
+function grid(gridSize){
+    let squareSize = (548 / gridSize) ;
+    
+    for(let i = 0; i < gridSize; i++){
+        const squareRow = document.createElement("div");
+        for(let i = 0; i < gridSize; i++){
+            const square = document.createElement("div");
+            square.style.height = `${squareSize}px`;
+            square.style.width = `${squareSize}px`;
             square.classList.add("square");
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "yellow";
+            })
             squareRow.appendChild(square);
         }
         container.appendChild(squareRow);
     }
 }
 
-grid(16,16);
+grid(16);
+gridBtn.addEventListener("click", () => {
+    let val = prompt("Please Insert the size you want:")
+    while(true){
+        if(val > 0 && val <= 100) {
+            container.textContent = "";
+            grid(val);
+            break
+        }else {
+            val = prompt("Please Enter a number between 0 and 100");
+        }
+    }
+});
 
 
 
